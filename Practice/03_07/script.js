e3278427ca90c5a5d13da1009e672b7ff71e7aee
Rdeaki <Rdeakins96@gmail.com>
2023-09-18 18:12:33 -0400

    created objects fishbowl ramranch
    yes

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,51 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+// const fishbowl = {
+//   name: "Large Aquarium",
+//   volume: 30,
+//   shape: "sqaure",
+//   fishTypeCount: {
+//     red: 3,
+//     blue: 4,
+//     striped: 6,
+//     octopus: 1,
+//   },
+//   lidOpen: false,
+//   toggLid: function (lidStatus) {
+//     this.lidOpen = lidStatus;
+//   },
+//   newFishCount: function (typeRed, typeBlue, typeStriped, typeOctopus) {
+//     this.fishTypeCount.red = typeRed;
+//     this.fishTypeCount.blue = typeBlue;
+//     this.fishTypeCount.striped = typeStriped;
+//     this.fishTypeCount.octopus = typeOctopus;
+//   },
+// };
+
+// console.log("The fishbowl object", fishbowl);
+// console.log("Red fish count", fishbowl.fishTypeCount.red);
+
+const ramranch = {
+  acres: 60,
+  stableCount: 6,
+  season: "summer",
+  animalCount: {
+    goat: 12,
+    horses: 8,
+    chicken: 13,
+    pigs: 10,
+    cows: 16,
+  },
+  newAnimalCount: function (gaotNum, horsesNum, chickenNum, pigsNum, cowsNum) {
+    this.animalCount.goat = gaotNum;
+    this.animalCount.horses = horsesNum;
+    this.animalCount.chicken = chickenNum;
+    this.animalCount.pigs = pigsNum;
+    this.animalCount.cows = cowsNum;
+  },
+};
+
+console.log("The ramranch object", ramranch);
+console.log("Chicken count", ramranch.animalCount.chicken);
